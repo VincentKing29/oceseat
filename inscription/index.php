46f61f9deab3b715910c6cf3f9963bd8c4b276ac
Joachim-B <93817125+Joachim-B@users.noreply.github.com>
2022-03-30 09:29:28 +0200

    Update index.php

--- inscription/index.php
+++ inscription/index.php
@@ -12,14 +12,9 @@
 </head>
 <body>
 
-    <header>
-        <div class="menu_logo">
-            <img src="../img/oceseat-header.png" alt="">
-        </div>
-        <div class="menu_camember">
-            <img src="../img/menu_burger.svg" alt="">
-        </div>
-    </header>
+    <?php
+        include("../php/header.php");
+    ?>
   
     <main>
 
